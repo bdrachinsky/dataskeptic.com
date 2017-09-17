2954e809c615e91c071758c356fa67b73efacb6c
looch <andrew@thespoon.co>
2017-09-17 23:23:28 +0300

    Start logic for new volume controller

--- shared/PlayerV2/Containers/VolumeContainer.js
+++ shared/PlayerV2/Containers/VolumeContainer.js
@@ -1,14 +1,16 @@
 import React,{Component} from 'react'
-
-
+import classNames from 'classnames'
 
 class VolumeContainer extends Component{
     constructor() {
         super();
         this.state = {
-            muted: false
+            muted: false,
+            current:undefined
         }
-        this.renderBars = this.renderBars.bind(this)
+        this.renderBars = this.renderBars.bind(this);
+        this.setCurrent = this.setCurrent.bind(this);
+        this.activeUnderCurrent = this.activeUnderCurrent.bind(this)
     }
     renderBars(quantity){
         let barArray = new Array(quantity);
@@ -16,14 +18,26 @@ class VolumeContainer extends Component{
         return barArray;
 
     }
+    setCurrent(index){
+        this.setState({current: index})
+    }
+    activeUnderCurrent(currentIndex,index){
+        if(index <= currentIndex ){
+            return true
+        }
+    }
 
     render(){
             const {quantity} = this.props;
+            const {current} = this.state;
           const bars = this.renderBars(quantity)
             return(
                 <div>
-                    <code>{JSON.stringify(quantity)}</code>
-                    {bars.map((b, index) => <div key={index}>{index}</div>)}
+                    {bars.map((b, index) => <div
+                        key={index}
+                        onClick={() => this.setCurrent(index)}
+                        className={classNames('volume-bar', {'active': this.activeUnderCurrent(current, index)})}>
+                        {index}</div>)}
                 </div>
             )
         }
